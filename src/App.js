5e1f288f2b8ad5b4d82c0611f5691ffa6f603791
Ellis-Brown <ellis.brown@tufts.edu>
2020-08-10 00:32:47 -0400

    Changed image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import "App.css";
-import sleepy from './images/Sleepy.png'
+import sleepy from './images/Sleepy.jpg';
+
+//Import components
+import Nav from './Nav';
+
 
 const App = () => (
     <div>
+        <Nav />
         <h1 className="thing"> Hello React</h1>
         <img src={sleepy} alt="sleepy" />
     </div>
