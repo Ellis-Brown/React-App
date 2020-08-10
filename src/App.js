4eac319089fa9083e3b2fc0fa55b73424b32651f
Ellis-Brown <ellis.brown@tufts.edu>
2020-08-10 00:29:49 -0400

    Added sleepy.png

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "App.css";
+import sleepy from './images/Sleepy.png'
 
 const App = () => (
     <div>
         <h1 className="thing"> Hello React</h1>
+        <img src={sleepy} alt="sleepy" />
     </div>
 )
 export default App;
