cb6b467a1958249a2a922c0cd0666070054a3cd0
Ellis-Brown <ellis.brown@tufts.edu>
2020-08-10 00:33:12 -0400

    Added nav

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => (
+   <nav>
+       <h1> Logo </h1>
+       <ul>
+           <li> LI 1 </li>
+           <li> LI 2 </li>
+       </ul>
+   </nav> 
+)
+export default Nav;
